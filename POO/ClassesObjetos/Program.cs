23ed2d72f369ae1fa09fefb12e509fe0cbccc482
Ana Clara <nininhazinha66779900@gmail.com>
2024-10-31 16:46:28 -0300

    nino

---
+++ POO/ClassesObjetos/Program.cs
@@ -0,0 +1,29 @@
+﻿//importanto as classes de dentro da pasta Classes 
+using ClassesObjetos.Classes;
+
+
+//cria/objeto/variável animal 
+Animal cachorro = new Animal();
+
+cachorro.nome = "Spaik";
+cachorro.raca = "Shihtzu";
+cachorro.cor = "Preto e Branco";
+cachorro.idade = "5"; 
+
+Console.WriteLine($"Nome do cachorrinho: {cachorro.nome}");
+Console.WriteLine($"Idade do cachorrinho: {cachorro.idade}");
+Console.WriteLine($"Cor do cachorrinho: {cachorro.cor}");
+Console.WriteLine($"Raca do cachorrinho: {cachorro.raca}");
+
+Animal gato = new Animal();
+
+gato.nome = "nino";
+gato.raca = "siames";
+gato.cor = "cinza";
+gato.idade = "1"; 
+
+Console.WriteLine($"Nome do gatinho: {gato.nome}"); 
+Console.WriteLine($"Idade do gatinho: {gato.idade}"); 
+Console.WriteLine($"Cor do gatinho: {gato.cor}"); 
+Console.WriteLine($"Raca do gatinho: {gato.raca}"); 
+
